747d4eeb9d1c318db481284d491e09b661a85723
Anubhav <aadhikari1248@gmail.com>
2023-05-02 01:50:38 +0530

    UI Tweaks

--- src/client/public/js/signup.js
+++ src/client/public/js/signup.js
@@ -1,71 +1,18 @@
-const form = document.getElementById('signup-form');
+document.querySelector('form[action="signUp.js"]').addEventListener('submit', function (e) {
+  e.preventDefault();
 
-function validateForm() {
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const reenterPassword = document.getElementById('reenter-password').value;
+  const form = e.target;
+  const username = form.uname.value;
+  const email = form.email.value;
+  const password = form.pass.value;
+  const passwordConfirm = form.pass_confirm.value;
 
-  // perform validation checks
-  if (name === '' || email === '' || password === '' || reenterPassword === '') {
-    alert('Please fill in all fields.');
-    return false;
-  }
-
-  if (password !== reenterPassword) {
-    alert('Passwords do not match.');
-    return false;
-  }
-
-  return true;
-}
-
-
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const reenterPassword = document.getElementById('reenter-password').value;
-
-  // perform validation checks
-  if (name === '' || email === '' || password === '' || reenterPassword === '') {
-    alert('Please fill in all fields.');
-    return;
-  }
-
-  if (password !== reenterPassword) {
+  if (password !== passwordConfirm) {
     alert('Passwords do not match.');
     return;
   }
 
-  // create an object with the form data
-  const data = {
-    name: name,
-    email: email,
-    password: password
-  };
+  // Perform your sign-up logic here (e.g., AJAX call to the server)
 
-  // make a fetch request to the server
-  fetch('/api/v1/user.js', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-    .then(response => {
-      if (response.ok) {
-        alert('Signup successful!');
-        //   window.location.href = '/dashboard';
-      } else {
-        throw new Error('Signup failed.');
-        console.log(response)
-      }
-    })
-    .catch(error => {
-      console.error(error);
-      alert('Signup failed.');
-    });
+  console.log('User registered:', { username, email, password });
 });
